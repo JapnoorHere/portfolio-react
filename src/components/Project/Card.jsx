7a9a781466d5badcbbcfce3f75110996efdd2b65
Japnoor Singh <sjapnoor11@gmail.com>
2024-06-18 16:16:54 +0530

    Made changes in card design

--- src/components/Project/Card.jsx
+++ src/components/Project/Card.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './Card.module.css'
-const Card = ({project}) => {
-
+const Card = ({ project }) => {
   const getImage = (imageName) => {
     try {
       return require(`../../assets/${imageName}`);
@@ -10,20 +9,25 @@ const Card = ({project}) => {
       return null;
     }
   };
-
-
+  var image = getImage(project.image)
+  var title = project.title
+  var code = project.code
+  var demo = project.demo
   return (
     <div className={styles.card}>
+
       <div className={styles.imgContainer}>
-        <img src={getImage(project.image)} alt="" />
-      </div>
-        <h3 className={styles.heading}>{project.title}</h3>
-        <p className={styles.description}>{project.description}</p>
-        {/* <div className={styles.tech}>{project.tech.split(",")}</div> */}
-        <div className={styles.projectLinks}>
-          <a target='_blank' href={project.demo} className={styles.demo}>Demo</a>
-          <a target='_blank' href={project.source} className={styles.source}>Source</a>
+
+        <img src={image} alt="" />
+        <div className={styles.imgOverlay}>
+          <i className='fa fa-plus'></i>
+          <h1>{title}</h1>
+          <div className={styles.links}>
+            <a target='_blank' href={demo}>View Demo</a>
+            <a target='_blank' href={code} >View Code</a>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
