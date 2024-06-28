ece3f2fe57e55e826a9ce42769c8f4f51307425c
Japnoor Singh <sjapnoor11@gmail.com>
2024-06-28 16:35:36 +0530

    Errors solved in a tage

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,22 +9,22 @@ const Contact = () => {
           <h1>Full Stack Developer</h1>
           <p>I am a Full Stack Developer skilled in creating dynamic web applications and intuitive mobile apps for iOS and Android. I also excel in UI/UX design, crafting visually appealing and user-friendly interfaces for both web and mobile platforms.</p>
           <div className={styles.socialLinks}>
-            <a href="https://mail.google.com/mail/?view=cm&to=sjapnoor11@gmail.com" target='_blank' className={styles.linkContainer}>
+            <a rel="noreferrer" href="https://mail.google.com/mail/?view=cm&to=sjapnoor11@gmail.com" target='_blank' className={styles.linkContainer}>
               <i className='fas fa-envelope'></i>
               </a> 
-              <a href='https://github.com/JapnoorHere' target='_blank' className={styles.linkContainer}>
+              <a rel="noreferrer" href='https://github.com/JapnoorHere' target='_blank' className={styles.linkContainer}>
                 <i className='fab fa-github'></i>
               </a>
-              <a href='https://www.linkedin.com/in/japnoor-singh/' target='_blank' className={styles.linkContainer}>
+              <a rel="noreferrer" href='https://www.linkedin.com/in/japnoor-singh/' target='_blank' className={styles.linkContainer}>
                 <i className='fab fa-linkedin'></i>
               </a>
-              <a href='https://www.youtube.com/channel/UCWMRbbyG5aFXQmqpG9zcMAw' target='_blank' className={styles.linkContainer}>
+              <a rel="noreferrer" href='https://www.youtube.com/channel/UCWMRbbyG5aFXQmqpG9zcMAw' target='_blank' className={styles.linkContainer}>
                 <i className='fab fa-youtube'></i>
               </a>
-              <a href='https://www.instagram.com/japnoor.here/' target='_blank' className={styles.linkContainer}>
+              <a rel="noreferrer" href='https://www.instagram.com/japnoor.here/' target='_blank' className={styles.linkContainer}>
                 <i className='fab fa-instagram'></i>
               </a>
-              <a href='https://x.com/japnoor_here' target='_blank' className={styles.linkContainer}>
+              <a rel="noreferrer" href='https://x.com/japnoor_here' target='_blank' className={styles.linkContainer}>
                 <i className='fab fa-x-twitter'></i>
               </a>
               
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Japnoor Singh</h1>
         <p className={styles.description}>I am a Full Stack Developer skilled in creating dynamic web applications and intuitive mobile apps for iOS and Android. I also excel in UI/UX design, crafting visually appealing and user-friendly interfaces for both web and mobile platforms.</p>
-        <a className={styles.resumeBtn} href="mailto:sjapnoor11@gmail.com">My CV</a>
+        <a className={styles.resumeBtn} href="/">My CV</a>
       </div>
         <img className={styles.heroImg} src={heroImg} alt="Hero Image" />
         <div className={styles.topBlur}></div>
