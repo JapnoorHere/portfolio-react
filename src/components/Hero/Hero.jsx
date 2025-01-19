cf1793f7e585ca986b2f5a5eb44298923595600e
Japnoor Singh <sjapnoor11@gmail.com>
2025-01-19 17:25:27 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Japnoor Singh</h1>
         <p className={styles.description}>I am a Full Stack Developer skilled in creating dynamic web applications and intuitive Android apps. I also excel in UI/UX design, for making user-friendly interfaces for both web and mobile platforms.</p>
-        <a className={styles.resumeBtn} href="https://drive.google.com/file/d/1cTnuEkzI9TYOCAKNd1Q3mIylFFF5Mocj/view?usp=drive_link" target='_blank' rel='noreferrer'>My CV</a>
+        <a className={styles.resumeBtn} href="https://drive.google.com/file/d/1V73ek066Ep6EE6V8e5G7WNXKQ1g47gfb/view?usp=drive_link" target='_blank' rel='noreferrer'>My CV</a>
       </div>
         <img className={styles.heroImg} src={heroImg} alt="Hero" />
         <div className={styles.topBlur}></div>
